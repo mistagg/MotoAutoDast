e190bf908fdd0ed2fca2d583cba51c7c32f4435c
PanConMay01 <132965669+PanConMay01@users.noreply.github.com>
2024-11-04 11:52:44 -0300

    Add files via upload

--- MainApp/urls.py
+++ MainApp/urls.py
@@ -5,6 +5,10 @@ from MainApp import views
 urlpatterns = [
     path('', views.inicio, name='inicio'),
     path('productos/', views.lista_productos, name='lista_productos'),
+    path('aceites/', views.Aceites, name='lista_aceites'),
+    path('accesorios/', views.Accesorios, name='lista_accesorios'),
+    path('neumaticos/', views.Neumaticos, name='lista_neumaticos'),
+    path('repuestos/', views.Repuestos, name='lista_repuestos'),
     path('accounts/', include('django.contrib.auth.urls')),
-
+    path('producto/<int:id>/', views.producto_detalle, name='producto_detalle'),
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .models import Producto
+from .models import Producto, Categoria
 # Create your views here.
 
 @login_required
@@ -10,3 +10,53 @@ def inicio(req):
 def lista_productos(request):
     productos = Producto.objects.all()
     return render(request, 'catalogoTest.html', {'productos': productos})
+
+from django.shortcuts import render, get_object_or_404
+from .models import Producto, Categoria
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def Aceites(req):
+    try:
+        categoria = Categoria.objects.get(nombre_categoria="Aceite")
+        productos = Producto.objects.filter(categoria=categoria)
+    except Categoria.DoesNotExist:
+        productos = []
+
+    return render(req, 'catalogo.html', {'productos': productos, 'categoria': 'Aceite'})
+
+@login_required
+def Accesorios(req):
+    try:
+        categoria = Categoria.objects.get(nombre_categoria="Accesorios")
+        productos = Producto.objects.filter(categoria=categoria)
+    except Categoria.DoesNotExist:
+        productos = []
+
+    return render(req, 'catalogo.html', {'productos': productos, 'categoria': 'Accesorios'})
+
+@login_required
+def Neumaticos(req):
+    try:
+        categoria = Categoria.objects.get(nombre_categoria="Neumaticos")
+        productos = Producto.objects.filter(categoria=categoria)
+    except Categoria.DoesNotExist:
+        productos = []
+
+    return render(req, 'catalogo.html', {'productos': productos, 'categoria': 'Neumáticos'})
+
+@login_required
+def Repuestos(req):
+    try:
+        categoria = Categoria.objects.get(nombre_categoria="Repuestos")
+        productos = Producto.objects.filter(categoria=categoria)
+    except Categoria.DoesNotExist:
+        productos = []
+
+    return render(req, 'catalogo.html', {'productos': productos, 'categoria': 'Repuestos'})
+
+
+@login_required
+def producto_detalle(req, id):
+    producto = get_object_or_404(Producto, id=id)
+    return render(req, 'producto_detalle.html', {'producto': producto})
