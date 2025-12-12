d9bd65639f7c79f7c5e8ffa04c3fd553bb0a27f3
Oliver Segovia <olivrsgvia@gmail.com>
2025-12-12 04:54:09 -0300

    Include root static/ folder in STATICFILES_DIRS

--- MotoAutoDast/settings.py
+++ MotoAutoDast/settings.py
@@ -21,6 +21,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 # Quick-start development settings - unsuitable for production
