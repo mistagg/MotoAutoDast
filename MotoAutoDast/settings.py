3ea4524f870cf9078ef21c7cb0ad845caa92c1d6
Oliver Segovia <olivrsgvia@gmail.com>
2025-12-12 05:47:29 -0300

    Fix STATICFILES_DIRS for Azure WhiteNoise

--- MotoAutoDast/settings.py
+++ MotoAutoDast/settings.py
@@ -21,7 +21,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
