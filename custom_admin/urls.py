bc4601424e74056dbbf2eb637ab9443ebd7ca215
Benja Espejo <benjamin.espejo03@inacapmail.cl>
2025-12-14 22:17:38 -0300

    Update urls.py

--- custom_admin/urls.py
+++ custom_admin/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import dashboard, admin_login, pagos_view
 from django.contrib.auth.views import LogoutView
-from MainApp.views import listar_productos, agregar_producto, editar_producto, eliminar_producto, ajustes
+from MainApp.views import listar_productos, agregar_producto, editar_producto, eliminar_producto
 
 urlpatterns = [
     path('', admin_login, name="admin_login"),  # Página de login
